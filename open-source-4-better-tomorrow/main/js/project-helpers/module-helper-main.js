4178e76fde3338f8bbb636e420ec36af9ded8534
open-source-4-better-tomorrow <open.source.4.better.tomorrow@gmail.com>
2017-08-13 12:05:58 +0200

    vCurrent
    Changes required for moving D-S-D to S-D-of-B-T

--- open-source-4-better-tomorrow/main/js/project-helpers/module-helper-main.js
+++ open-source-4-better-tomorrow/main/js/project-helpers/module-helper-main.js
@@ -36,7 +36,7 @@
 
     var _projectDescriptionRedirectionUrl = "/open-source-4-better-tomorrow/project_description/";
 	
-    var _DSD_RedirectionUrl = "https://dabrowski-software-development.github.io";
+    var _DSD_RedirectionUrl = "https://software-development-of-better-tomorrow.github.io";
     
     var _downloadCode_RedirectionUrl = "https://github.com/Open-Source-4-Better-Tomorrow/Open-Source-4-Better-Tomorrow.github.io/archive/master.zip";
 
